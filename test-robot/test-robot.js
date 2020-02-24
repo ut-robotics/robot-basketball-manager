3979967b4d52ae477ca7b4b16586564a7bcbc2cf
Reiko Randoja <reiko.randoja@gmail.com>
2020-02-24 11:36:44 +0200

    Improve test robot

--- test-robot/test-robot.js
+++ test-robot/test-robot.js
@@ -1,11 +1,28 @@
-const WebSocket = require('ws');
+const WebsocketManager = require('./websocket-manager');
+const wsManager = new WebsocketManager('localhost:8111');
+const robotID = '001trt';
 
-const ws = new WebSocket('ws://localhost:8111');
-
-ws.on('open', function open() {
-    ws.send(JSON.stringify({method: 'get_active_game_state', id: 1}));
+wsManager.on('open', () => {
+    wsManager.send({method: 'get_active_game_state', id: 1});
 });
 
-ws.on('message', function incoming(data) {
-    console.log(data);
-});
+wsManager.on('message', onMessage);
+
+function onMessage(message) {
+    console.log(message);
+
+    try {
+        const info = JSON.parse(message);
+        const {signal, targets} = info;
+
+        if (Array.isArray(targets) && targets.includes(robotID)) {
+            if (signal === 'stop') {
+                console.log('STOP');
+            } else if (signal === 'start') {
+                console.log('START');
+            }
+        }
+    } catch (e) {
+        console.error(e);
+    }
+}
