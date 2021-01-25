4fd2597789cd70222f0c62e68bf2b169d101eb92
Reiko Randoja <reiko.randoja@gmail.com>
2021-01-25 10:09:51 +0200

    Revert referee remote changes in basket score counter firmware

--- arduino/basket_score_counter/basket_score_counter.ino
+++ arduino/basket_score_counter/basket_score_counter.ino
@@ -7,12 +7,12 @@ using namespace websockets;
 unsigned long reconnectDelay = 1000;
 unsigned long lastReconnectTime = 0;
 
-const int sensorPin = 22;
-int sensorState = HIGH;
-int lastSensorState = HIGH;
+const int sensorPin = 23;
+int sensorState = LOW;
+int lastSensorState = LOW;
 
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
-unsigned long debounceDelay = 10;
+unsigned long debounceDelay = 5;
 
 const int blueLedPin = 25;
 const int orangeLedPin = 26;
@@ -61,7 +61,7 @@ void onEventsCallback(WebsocketsEvent event, String data) {
 void setup() {
     Serial.begin(115200);
 
-    pinMode(sensorPin, INPUT_PULLUP);
+    pinMode(sensorPin, INPUT);
     pinMode(blueLedPin, OUTPUT);
     pinMode(orangeLedPin, OUTPUT);
     pinMode(greenLedPin, OUTPUT);
@@ -120,7 +120,11 @@ void loop() {
         int currentBasketSelection = digitalRead(basketSelectionPin);
 
         if (sensorState == HIGH) {
-          client.send("start_stop");
+          if (currentBasketSelection == HIGH) {
+            client.send("magenta");
+          } else {
+            client.send("blue");
+          }
         }
        }
     }
