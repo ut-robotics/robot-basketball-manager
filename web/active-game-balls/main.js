b8c419bf3fd99d489e11ac3a48bd068204497b92
Reiko Randoja <reiko.randoja@gmail.com>
2021-01-27 07:18:20 +0200

    Flip y coordinate when drawing ball placement

--- web/active-game-balls/main.js
+++ web/active-game-balls/main.js
@@ -38,7 +38,7 @@ function renderState(state) {
     console.log(ballPlacement);
 
     boxElement.innerHTML = ballPlacement
-        .map(point => `<div class="dot" style="left: ${point[0] * 100 + 225}px;top: ${point[1] * 100 + 150}px"></div>`)
+        .map(point => `<div class="dot" style="left: ${point[0] * 100 + 225}px;top: ${-point[1] * 100 + 150}px"></div>`)
         .join('');
 }
 
